3cfe9f7b5900b2456d0be01db6754f7f5bd3c526
闫韬蒙 <3094969938@qq.com>
2022-07-20 19:52:51 +0800

    feat: 添加了初始化数据的代码

--- min-vue/dist/vue.js
+++ min-vue/dist/vue.js
@@ -11,9 +11,25 @@
 
   // 给Vue增加初始化方法
   function initMixin(Vue) {
-    Vue.prototype._init = function () {
-      console.log("hello");
+    Vue.prototype._init = function (options) {
+      var vm = this;
+      vm.$options = options;
+      initState(vm);
     };
+  }
+
+  function initState(vm) {
+    var opts = vm.$options;
+
+    if (opts.data) {
+      initData(vm);
+    }
+  }
+
+  function initData(vm) {
+    var data = vm.$options.data;
+    data = typeof data == "function" ? data.call(vm) : data;
+    console.log(data);
   }
 
   function Vue(options) {
--- min-vue/src/init.js
+++ min-vue/src/init.js
@@ -1,6 +1,20 @@
 // 给Vue增加初始化方法
 export function initMixin(Vue) {
-  Vue.prototype._init = function () {
-    console.log("hello");
+  Vue.prototype._init = function (options) {
+    const vm = this;
+    vm.$options = options;
+    initState(vm);
   };
+}
+function initState(vm) {
+  const opts = vm.$options;
+  if (opts.data) {
+    initData(vm);
+  }
+}
+
+function initData(vm) {
+  let { data } = vm.$options;
+  data = typeof data == "function" ? data.call(vm) : data;
+  console.log(data);
 }
