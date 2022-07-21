3cec27368e4ef52e00268de50ea2cacba1d4d490
闫韬蒙 <3094969938@qq.com>
2022-07-21 21:09:49 +0800

    feat: 添加了解析模板参数的代码

--- min-vue/dist/vue.js
+++ min-vue/dist/vue.js
@@ -208,14 +208,51 @@
     }
   }
 
+  function complieToFunction() {}
+
   // 给Vue增加初始化方法
   function initMixin(Vue) {
     Vue.prototype._init = function (options) {
       var vm = this;
       vm.$options = options;
       initState(vm);
+
+      if (options.el) {
+        vm.$mount(options.el);
+      }
     };
-  }
+
+    Vue.prototype.$mount = function (el) {
+      var vm = this;
+      el = document.querySelector(el);
+      var opts = vm.$options;
+      console.log(opts); // 当没有render函数时
+
+      if (!(opts !== null && opts !== void 0 && opts.render)) {
+        var template = null; // 没有模板但写了el
+
+        if (!opts.template && opts.el) {
+          template = el.outerHTML; // 存在模板或不存在模板和el
+        } else {
+          // 存在模板
+          if (opts.template) {
+            template = opts.template;
+          } else {
+            // 既没有模板也没有el
+            template = "<div></div>";
+          }
+        } //拿到模板
+        // 获取优先级：render=>temple=>el
+
+        if (template) {
+          var render = complieToFunction();
+          opts.render = render;
+        }
+      }
+    };
+  } // script标签引入的vue，编译过程在浏览器
+  // runtime是不包含编译的，编译时通过loader进行转译.vue文件，用runtime不能使用template(在mian.js中，.vue文件有loader转译，因此不影响)
+  // 包含main.js中模板编译的版本是runtime-with-compiler
 
   function Vue(options) {
     this._init(options);
--- min-vue/src/init.js
+++ min-vue/src/init.js
@@ -1,9 +1,45 @@
 // 给Vue增加初始化方法
 import { initState } from "./state";
+import { complieToFunction } from "./compiler/index";
 export function initMixin(Vue) {
   Vue.prototype._init = function (options) {
     const vm = this;
     vm.$options = options;
     initState(vm);
+    if (options.el) {
+      vm.$mount(options.el);
+    }
+  };
+  Vue.prototype.$mount = function (el) {
+    const vm = this;
+    el = document.querySelector(el);
+    let opts = vm.$options;
+    console.log(opts);
+    // 当没有render函数时
+    if (!opts?.render) {
+      let template = null;
+      // 没有模板但写了el
+      if (!opts.template && opts.el) {
+        template = el.outerHTML;
+        // 存在模板或不存在模板和el
+      } else {
+        // 存在模板
+        if (opts.template) {
+          template = opts.template;
+        } else {
+          // 既没有模板也没有el
+          template = "<div></div>";
+        }
+      }
+      //拿到模板
+      // 获取优先级：render=>temple=>el
+      if (template) {
+        const render = complieToFunction(template);
+        opts.render = render;
+      }
+    }
   };
 }
+// script标签引入的vue，编译过程在浏览器
+// runtime是不包含编译的，编译时通过loader进行转译.vue文件，用runtime不能使用template(在mian.js中，.vue文件有loader转译，因此不影响)
+// 包含main.js中模板编译的版本是runtime-with-compiler
